a1c388ae5585add3eeedcb080ee70f15182398bb
astelyn <allie@fcraft.net>
2013-07-10 23:12:21 -0400

    MMU

--- src/mmu/mmu.go
+++ src/mmu/mmu.go
@@ -1,23 +1,34 @@
 package mmu
 
-type MMU struct {
+type MMU [0x10000]uint16
 
+func NewMMU() *MMU {
+	mmu := new(MMU)
+	mmu.Init()
+	return mmu
+}
+
+func (mmu *MMU) Init() {
+	for i, _ := range mmu {
+		mmu[i] = 0
+	}
 }
 
 func (mmu *MMU) ReadByte(addr uint16) byte {
-	// TODO: implement
-	return 0x00
+	return mmu[addr]
 }
 
 func (mmu *MMU) ReadWord(addr uint16) uint16 {
-	// TODO: implement
-	return 0x0000
+	return uint16(mmu.ReadByte(addr)) |
+		(uint16(mmu.ReadByte(addr+1)) << 8)
 }
 
 func (mmu *MMU) WriteByte(addr uint16, val byte) {
-	// TODO: implement
+	mmu[addr] = val
 }
 
 func (mmu *MMU) WriteWord(addr, val uint16) {
-	// TODO: implement
+	mmu.WriteByte(addr, byte(val&0xFF))
+	mmu.WriteByte(addr+1, byte((val>>8)&0xFF))
 }
+
